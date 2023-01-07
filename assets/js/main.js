360170536351e103e5bc01cb9460838f0259b4c6
Tusar <tusarahmed141@gmail.com>
2023-01-07 16:55:20 +0600

    Toggle button don e

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,29 @@
 // Fixed Navigation
 const nav = document.querySelector(".nav");
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (scrollY >= 80) {
-    nav.classList.add('fixedNav');
+    nav.classList.add("fixedNav");
   } else {
-    nav.classList.remove('fixedNav');
+    nav.classList.remove("fixedNav");
   }
 });
 
+// Navigation Toggle
+const navMenu = document.getElementById("nav-menu");
+const toggleBtn = document.querySelector(".toggleBtn");
+const toggleIcon = document.querySelector(".toggle__icon");
+console.log(toggleIcon);
 
+toggleBtn.addEventListener("click", () => {
+  if (toggleIcon.className == 'bx bx-menu toggle__icon') {
+    toggleIcon.className = "bx bx-x toggle__icon";
+    navMenu.style.width = '100%';
+    navMenu.style.left = '0';
+    navMenu.style.paddingLeft = '5vw';
+  } else {
+    toggleIcon.className = "bx bx-menu toggle__icon";
+    navMenu.style.width = '0';
+    navMenu.style.left = '-20%';
+    navMenu.style.paddingLeft = '0';
+  }
+});
