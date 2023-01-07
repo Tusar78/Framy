bbd988273a8d9e734a3d7e899556f8a570ce83dd
Tusar <tusarahmed141@gmail.com>
2023-01-07 12:57:14 +0600

    toggle button possitioned done

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const shop = document.querySelector('.shop');
+const dropdown = document.querySelector('.dropdown'); 
+
+// shop.addEventListener('click', () => {
+//   dropdown.style.left = '0%';
+// })
