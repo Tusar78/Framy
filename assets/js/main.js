ee7a4bd82785f931e2ee59766cb98d79b19a8300
Tusar <tusarahmed141@gmail.com>
2023-01-07 13:28:28 +0600

    Really effective done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,15 @@
-const shop = document.querySelector('.shop');
-const dropdown = document.querySelector('.dropdown'); 
+const nav = document.querySelector(".nav");
+const dropdown = document.querySelector(".dropdown");
+const body = document.querySelector('body');
 
 // shop.addEventListener('click', () => {
 //   dropdown.style.left = '0%';
 // })
 
-
-$(window).scroll(function(){
-  if ($(window).scrollTop() >= 300) {
-      $('nav').addClass('fixed-header');
-      $('nav div').addClass('visible-title');
-  }
-  else {
-      $('nav').removeClass('fixed-header');
-      $('nav div').removeClass('visible-title');
+window.addEventListener('scroll', () => {
+  if (scrollY >= 300) {
+    console.log("Hello");
+  } else {
+    console.log("Gello");
   }
 });
