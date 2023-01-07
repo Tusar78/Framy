bfa49cef4b86a74737422478b63bcad64b142eb0
Tusar <tusarahmed141@gmail.com>
2023-01-07 13:01:25 +0600

    Simple update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,15 @@ const dropdown = document.querySelector('.dropdown');
 // shop.addEventListener('click', () => {
 //   dropdown.style.left = '0%';
 // })
+
+
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 300) {
+      $('nav').addClass('fixed-header');
+      $('nav div').addClass('visible-title');
+  }
+  else {
+      $('nav').removeClass('fixed-header');
+      $('nav div').removeClass('visible-title');
+  }
+});
